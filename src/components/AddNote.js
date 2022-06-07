2498adc4349ce996706d520a2e7f50c737f98329
Lina Zoo <linazooper@gmail.com>
2022-06-07 14:06:48 -0500

    added new notes component

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,10 +1,33 @@
-const AddNote = () => {
+import * as React from "react";
+
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = React.useState("");
+
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+
+  const handleSaveClick = () => {
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText("");
+    }
+  };
+
   return (
     <div className="note new">
-      <textarea rows="8" cols="10" placeholder="Add a new note"></textarea>
+      <textarea
+        onChange={handleChange}
+        rows="8"
+        cols="10"
+        placeholder="Add a new note"
+        value={noteText}
+      ></textarea>
       <div className="note-footer">
         <small>200 remaining chars</small>
-        <button className="save">Save</button>
+        <button onClick={handleSaveClick} className="save">
+          Save
+        </button>
       </div>
     </div>
   );
