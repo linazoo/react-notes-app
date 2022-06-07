848f22c3bb4448031adb4e691fe157e71d9b5cbf
Lina Zoo <linazooper@gmail.com>
2022-06-07 11:26:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = React.useState([
+    {
+      id: nanoid(),
+      text: "this is my first note",
+      date: "05/22/21",
+    },
+    {
+      id: nanoid(),
+      text: "this is my second note",
+      date: "04/4/21",
+    },
+    {
+      id: nanoid(),
+      text: "this is my third note",
+      date: "06/3/21",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NotesList notes={notes} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NotesList.js
@@ -0,0 +1,12 @@
+import Note from "./Note";
+
+const NotesList = ({ notes }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Note id={note.id} text={note.text} date={note.date} />
+      ))}
+    </div>
+  );
+};
+export default NotesList;
